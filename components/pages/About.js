1d0e47dcedfaaa4efb3671e19ade6854f0b7e2c5
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-16 16:52:20 -0400

    set up about page

---
+++ components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return(
+        <div class="jumbotron jumbotron-fluid custom-jumbo">
+            <div class="container text-center bg-light custom-container">
+                <h1 class="display-4">About</h1>
+                <p>I am a full-stack developer currently enrolled in the full-time bootcamp at Rutgers University! I come from an extensive background of biology and healthcare. I graduated from Oakland University with a B.S. in Biomedical Science in May 2021, and worked in clinical medicine for four years. I am interested in creating applications for use in healthcare or STEM! I am very knowledgeable in both the healthcare and biological research industries.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
