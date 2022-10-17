1edcc8abe7cfb82d5d9fff8c7c0eb5cc2d3cf6b3
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-17 14:22:38 -0400

    styled resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React from "react";
+import ResumeDoc from "../../Documents/Resume.pdf";
+import '../../styles/resume.css'
 
-//TODO: add list of proficiencies
-function Resume () {
-    return(
-        <div class="d-flex">
-            <embed src="../Resume.pdf" width="100%" height="1000px" />
-        </div>
-    )
+//TODO: add more proficiencies
+//TODO: adjust stack order (responsive UI design), improve colors
+function Resume() {
+  return (
+    <div class="d-flex">
+      <embed src={ResumeDoc} width="100%" height="1000px" />
+      <div class="card cust-card">
+        <div class="card-header">Proficiencies</div>
+        <h5 class="card-title">Programming Languages</h5>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">JavaScript</li>
+          <li class="list-group-item">Python</li>
+          <li class="list-group-item">C#</li>
+        </ul>
+
+        <h5 class="card-title">Back End</h5>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">MySQL</li>
+          <li class="list-group-item">NoSQL</li>
+        </ul>
+
+        <h5 class="card-title">Front End</h5>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">React</li>
+          <li class="list-group-item">Node.js</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Resume;
