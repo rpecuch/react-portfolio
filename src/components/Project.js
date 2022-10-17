9ffc0d58bf7fe91eb55c46452b24b0be6ae5ed5b
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-17 13:45:21 -0400

    added styling to work samples page but found bug

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-//TODO: only some of them are supposed to have the class custom caption
 function Project ({image, alt, link, title, description, github, tech}) {
  return(
     <div class={alt === 'kids rx application' ? "carousel-item active" : "carousel-item"}>
-        <img src={`./images/${image}`} class="d-block w-100" alt={alt}/>
+        <img src={image} class="d-block w-100" alt={alt}/>
         <div class="carousel-caption d-none d-md-block text-dark custom-caption">
             <a href={link} class="text-dark"><h5>{title}</h5></a>
             <p>{description}.</p>
--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,91 +1,109 @@
 import React from 'react';
 import Project from '../Project';
+import '../../styles/work.css'
+import kidsPic from '../../images/kids-rx.png';
+import partyPic from '../../images/virtual-party.png';
+import quizPic from '../../images/js-quiz.png';
+import schedulePic from '../../images/work-scheduler.png';
+import weatherPic from '../../images/weather.png';
+import notePic from '../../images/note-taker.png';
+import teamPic from '../../images/team-generator.png';
+import empPic from '../../images/emp-db.png';
 
 //TODO: add newer projects
+//! previous and next buttons not working
 function Work () {
     const kidsRx = {
-        image: 'kids-rx.png',
+        image: kidsPic,
         alt: 'kids rx application',
         link: "https://kids-rx-emr.herokuapp.com/",
         title: "Kids RX: Pediatric Electronic Medical Records",
         description: "A full-stack application that allows providers to keep track of patient records. Like other EMR systems, features include appointment scheduling, creating patient charts, writing medical record notes, logging vital signs, and logging vaccination records. What makes Kids RX different is that it is specifically designed for use in pediatrics and includes features that were implemented with children in mind.",
         github: "https://github.com/rpecuch",
-        tech: "Created using HTML, Tailwind CSS, Bootstrap, custom CSS, JavaScript, Node.js, Express, MySQL, Sequelize, Moment.js, Font Awesome, and Google Fonts. Note: this is a private repo."
+        tech: "Created using HTML, Tailwind CSS, Bootstrap, custom CSS, JavaScript, Node.js, Express, MySQL, Sequelize, Moment.js, Font Awesome, and Google Fonts. Note: this is a private repo.",
+        class: "carousel-caption d-none d-md-block text-dark custom-caption"
     }
 
     const party = {
-        image: "virtual-party.png",
+        image: partyPic,
         alt: "virtual party mom application",
         link: "https://rpecuch.github.io/virtual-party/",
         title: "Virtual Party Mom",
         description: "A tool that generates activities for group gatherings based on the current weather conditions. In-app features include a joke generator, Cards Against Humanity, and Would You Rather?!",
         github: "https://github.com/rpecuch/virtual-party",
-        tech: "Created using HTML, Tailwind CSS, Bootstrap, custom CSS, JavaScript, jQuery, Moment.js, Font Awesome, and Google Fonts."
+        tech: "Created using HTML, Tailwind CSS, Bootstrap, custom CSS, JavaScript, jQuery, Moment.js, Font Awesome, and Google Fonts.",
+        class: "carousel-caption d-none d-md-block text-dark custom-caption"
     }
 
     const quiz = {
-        image: "js-quiz.png",
+        image: quizPic,
         alt: "javascript quiz application",
         link: "https://rpecuch.github.io/javascript-quiz/",
         title: "JavaScript Coding Quiz",
         description: "An interactive timed quiz about JavaScript concepts.",
         github: "https://github.com/rpecuch/javascript-quiz",
-        tech: "Created using HTML, custom CSS, and JavaScript."
+        tech: "Created using HTML, custom CSS, and JavaScript.",
+        class: "carousel-caption d-none d-md-block text-dark"
     }
 
     const schedule = {
-        image: "work-scheduler.png",
+        image: schedulePic,
         alt: "work day scheduler application",
         link: "https://rpecuch.github.io/work-day-scheduler/",
         title: "Work Day Scheduler",
         description: "An application to create and save events for the work day.",
         github: "https://github.com/rpecuch/work-day-scheduler",
-        tech: "Created using HTML, Bootstrap, custom CSS, JavaScript, jQuery, Moment.js, Font Awesome, and Google Fonts."
+        tech: "Created using HTML, Bootstrap, custom CSS, JavaScript, jQuery, Moment.js, Font Awesome, and Google Fonts.",
+        class: "carousel-caption d-none d-md-block text-dark custom-caption"
     }
 
     const weather = {
-        image: "weather.png",
+        image: weatherPic,
         alt: "weather dashboard application",
         link: "https://rpecuch.github.io/weather-dashboard/",
         title: "Weather Dashboard",
         description: "An application to display the weather forecast for a city that is searched by the user",
         github: "https://github.com/rpecuch/weather-dashboard",
-        tech: "Created using HTML, Bootstrap, custom CSS, JavaScript, Moment.js, Font Awesome, and Google Fonts."
+        tech: "Created using HTML, Bootstrap, custom CSS, JavaScript, Moment.js, Font Awesome, and Google Fonts.",
+        class: "carousel-caption d-none d-md-block text-dark"
     }
 
     const note = {
-        image: "note-taker.png",
+        image: notePic,
         alt: "note taker application",
         link: "https://express-note-taker-rp.herokuapp.com/",
         title: "Note Taker",
         description: "A full-stack application to allow the user to write, save, and delete notes",
         github: "https://github.com/rpecuch/note-taker",
-        tech: "Created using HTML, Bootstrap, custom CSS, JavaScript, Node.js, and Express."
+        tech: "Created using HTML, Bootstrap, custom CSS, JavaScript, Node.js, and Express.",
+        class: "carousel-caption d-none d-md-block text-dark"
     }
 
     const team = {
-        image: "team-generator.png",
+        image: teamPic,
         alt: "team profile generator application",
         link: "https://github.com/rpecuch/team-profile-generator",
         title: "Team Profile Generator",
         description: "A command-line application to allow the user to generate an HTML document with information about a software engineering team.",
         github: "https://github.com/rpecuch/team-profile-generator",
-        tech: "Created using JavaScript, Node.js, HTML, Bootstrap, and Font Awesome."
+        tech: "Created using JavaScript, Node.js, HTML, Bootstrap, and Font Awesome.",
+        class: "carousel-caption d-none d-md-block text-dark"
     }
 
     const empDb = {
-        image: "emp-db.png",
+        image: empPic,
         alt: "employee database application",
         link: "https://github.com/rpecuch/employee-database",
         title: "Employee Database",
         description: "A command-line application to allow the user to view and modify an employee database.",
         github: "https://github.com/rpecuch/employee-database",
-        tech: "Created using JavaScript, Node.js, and MySQL."
+        tech: "Created using JavaScript, Node.js, and MySQL.",
+        class: "carousel-caption d-none d-md-block text-dark"
     }
 
     return(
         <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
+        <ol class="carousel-indicators custom-ol">
           <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
           <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
           <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
