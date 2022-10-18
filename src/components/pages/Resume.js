8720938f823525a340f89419fd36dcfd53d0e493
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-17 20:32:41 -0400

    added proficiencies to resume section

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -9,7 +9,7 @@ function Resume() {
     <div class="d-flex">
       <embed src={ResumeDoc} width="100%" height="1000px" />
       <div class="card cust-card">
-        <div class="card-header">Proficiencies</div>
+        <div class="card-header"><h3>Proficiencies</h3></div>
         <h5 class="card-title">Programming Languages</h5>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">JavaScript</li>
@@ -19,14 +19,23 @@ function Resume() {
 
         <h5 class="card-title">Back End</h5>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item">MySQL</li>
-          <li class="list-group-item">NoSQL</li>
+          <li class="list-group-item">MySQL, Sequelize</li>
+          <li class="list-group-item">MongoDB, Mongoose</li>
+          <li class="list-group-item">Express</li>
+          <li class="list-group-item">Node.js</li>
+          <li class="list-group-item">Restful APIs</li>
+          <li class="list-group-item">GraphQL</li>
         </ul>
 
         <h5 class="card-title">Front End</h5>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">React</li>
-          <li class="list-group-item">Node.js</li>
+          <li class="list-group-item">DOM Manipulation</li>
+          <li class="list-group-item">HTML</li>
+          <li class="list-group-item">CSS</li>
+          <li class="list-group-item">Bootstrap</li>
+          <li class="list-group-item">jQuery</li>
+          <li class="list-group-item">Responsive Design</li>
         </ul>
       </div>
     </div>
