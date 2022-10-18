710a1feb64653fa3e5e4c0f99428fc5b90b9b799
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-18 11:08:04 -0400

    resized social icons and added link to stack overflow acct

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
-//TODO: add link to profile on third website
 //TODO: make everything larger, responsive UI
 function Footer () {
     const styles = {
@@ -10,9 +9,7 @@ function Footer () {
             background: '#d1d1e0',
             display: 'flex',
             flexDirection: 'column',
-        },
-        p: {
-            marginTop: '2%'
+            padding: '2%'
         },
         ul: {
             display: 'flex',
@@ -27,10 +24,11 @@ function Footer () {
 
     return(
         <footer style={styles.footer}>
-            <p class="text-center" style={styles.p}>Thank you for visiting!</p>
+            <p class="text-center">Thank you for visiting!</p>
             <ul style={styles.ul}>
-                <li style={styles.li}><a href="https://github.com/rpecuch"><FontAwesomeIcon icon={faGithub} style={styles.li}/></a></li>
-                <li style={styles.li}><a href="https://www.linkedin.com/in/rita-pecuch/"><FontAwesomeIcon icon={faLinkedin} style={styles.li}/></a></li>
+                <li style={styles.li}><a href="https://github.com/rpecuch"><FontAwesomeIcon icon={faGithub} size="2xl" style={styles.li}/></a></li>
+                <li style={styles.li}><a href="https://www.linkedin.com/in/rita-pecuch/"><FontAwesomeIcon icon={faLinkedin} size="2xl" style={styles.li}/></a></li>
+                <li style={styles.li}><a href="https://stackoverflow.com/users/20274103/rita-pecuch"><FontAwesomeIcon icon={faStackOverflow} size="2xl" style={styles.li}/></a></li>
           </ul>
         </footer>
     )
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/header.css';
+
 
 function Navigation({ page, changePage}) {
     const styles = {
