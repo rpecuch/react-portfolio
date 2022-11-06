4d4e48c164fbdee17e961213d367a359d006f04b
userrp <109092365+rpecuch@users.noreply.github.com>
2022-11-06 11:16:40 -0500

    updated contact page got rid of form for now as it is not functional

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,62 +1,63 @@
-import React, { useState } from 'react';
+import React from 'react';
 //import stylesheet
 import '../../styles/contact.css';
 //import font awesome icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Contact () {
-    const [contactName, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [confMessage, setConfMessage] = useState('');
+    // const [contactName, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [message, setMessage] = useState('');
+    // const [confMessage, setConfMessage] = useState('');
     //runs whenever input in any of the form input areas changes
-    const inputChange = (e) => {
-        //extract e.target.name and e.target.value
-        const { name, value } = e.target;
-        //set value of state variable that corresponds with the input field that was edited
-        if (name === 'name') {
-            setName(value);
-            setConfMessage('');
-        }
-        else if (name === 'email') {
-            setEmail(value);
-            setConfMessage('');
-        }
-        else {
-            setMessage(value);
-            setConfMessage('');
-        }
-        //displays message if an input field is left blank
-        if (!contactName) {
-            setConfMessage('Name is required')
-        }
-        if (!email) {
-            setConfMessage('Email is required')
-        }
-        if (!message) {
-            setConfMessage('Message is required')
-        }
-    }
+    // const inputChange = (e) => {
+    //     //extract e.target.name and e.target.value
+    //     const { name, value } = e.target;
+    //     //set value of state variable that corresponds with the input field that was edited
+    //     if (name === 'name') {
+    //         setName(value);
+    //         setConfMessage('');
+    //     }
+    //     else if (name === 'email') {
+    //         setEmail(value);
+    //         setConfMessage('');
+    //     }
+    //     else {
+    //         setMessage(value);
+    //         setConfMessage('');
+    //     }
+    //     //displays message if an input field is left blank
+    //     if (!contactName) {
+    //         setConfMessage('Name is required')
+    //     }
+    //     if (!email) {
+    //         setConfMessage('Email is required')
+    //     }
+    //     if (!message) {
+    //         setConfMessage('Message is required')
+    //     }
+    // }
     //runs when submit button is clicked
-    const formSubmit = (e) => {
-        e.preventDefault();
-        //displays message if any field is left blank
-        if(!contactName || !email || !message) {
-            setConfMessage('Fill out all required fields!');
-        }
-        //displays message if email is not in correct format
-        else if (!email.match(/^([a-z0-9_.-]+)@([\da-z.-]+)\.([a-z.]{2,6})$/)) {
-            setConfMessage('Invalid email format!')
-        }
-        //success message displayed and clears input fields
-        else {
-            setConfMessage('Message received!');
-            setName('');
-            setEmail('');
-            setMessage('');
-        }
-    }
+    // const formSubmit = (e) => {
+    //     e.preventDefault();
+    //     //displays message if any field is left blank
+    //     if(!contactName || !email || !message) {
+    //         setConfMessage('Fill out all required fields!');
+    //     }
+    //     //displays message if email is not in correct format
+    //     else if (!email.match(/^([a-z0-9_.-]+)@([\da-z.-]+)\.([a-z.]{2,6})$/)) {
+    //         setConfMessage('Invalid email format!')
+    //     }
+    //     //success message displayed and clears input fields
+    //     else {
+    //         setConfMessage('Message received!');
+    //         setName('');
+    //         setEmail('');
+    //         setMessage('');
+    //     }
+    // }
     //define custom styles (in addition to stylesheet)
     const styles = {
         icon: {
@@ -83,9 +84,11 @@ function Contact () {
           <ul>
             <li style={styles.li}><FontAwesomeIcon icon={faPhone} size='lg' style={styles.icon}/>517-375-7765</li>
             <li id="email" style={styles.li}><FontAwesomeIcon icon={faEnvelope} size="lg" style={styles.icon}/><a href="mailto:rpecuch@comcast.net" style={styles.a} >rpecuch@comcast.net</a></li>
+            <li id='linkedin' style={styles.li}><FontAwesomeIcon icon={faLinkedin} size="lg" style={styles.icon}/><a href="https://www.linkedin.com/in/rita-pecuch" style={styles.a} >https://www.linkedin.com/in/rita-pecuch</a></li>
+            <li id='github' style={styles.li}><FontAwesomeIcon icon={faGithub} size="lg" style={styles.icon}/><a href="https://github.com/rpecuch" style={styles.a} >https://github.com/rpecuch</a></li>
           </ul>
 
-          <form>
+          {/* <form>
             <h2 style={styles.heading}>Contact Me</h2>
             <div className="form-group">
                 <label htmlFor="name" style={styles.li}>Name:</label>
@@ -101,7 +104,7 @@ function Contact () {
             </div>
             <p style={styles.li}>{confMessage}</p>
             <button type="button" onClick={formSubmit}>Submit</button>
-        </form>
+        </form> */}
         </div>
         
     </div>
